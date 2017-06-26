9cc09456c955c541773f0de465000ccca6056f27
wolfgang-wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2017-06-26 15:29:06 +0200

    Ausgangszustand Beispiele und Create-Skript zur Anlage der Demodatenbank

---
+++ demodatenbank_beispiele.sql
@@ -0,0 +1,102 @@
+﻿/*
+ * SQLs ohne Join
+ */
+-- Ausgeben der Inhalte von student und pruefung
+select * from student;
+select * from pruefung;
+
+-- Nur die ersten 3 Zeilen ausgeben
+select *
+from student
+limit 3;
+
+-- Ungleich
+select * from leistung
+where student_id != 10;
+
+select * from leistung
+where student_id <> 10;
+
+--  Ermitteln der Anzahl der Studenten
+select count(*) from student;
+
+-- Ermitteln der Anzahl der männlichen und weiblichen Studenten
+select geschlecht, count(*) as anzahl
+from student
+group by geschlecht;
+
+-- Prüfungen nach Prüfungsnummer (und damit nach Semester) sortiert
+select * from pruefung 
+order by pnr;
+
+-- Anzahl der Prüfungen pro Lehrplansemester
+select (pnr/10)-99 as lpsem, count(*)
+from pruefung
+group by (pnr/10)-99
+order by lpsem;
+
+-- Ermitteln aller Prüfungen im 2. LPSem
+select *
+from pruefung
+where cast(pnr as varchar(10)) like '__1_';
+
+-- Warum ist das folgende Statement nicht OK?
+select *
+from pruefung
+where cast(pnr as varchar(10)) like '%1%';
+
+-- Was liefert dieses Statement?
+select *
+from pruefung
+where cast(pnr as varchar(10)) similar to '[0-9][0-9][12][0-9]';
+
+/*
+ * SQLs mit Join
+ */
+-- Ausgabe aller Studenten mit ihren Prüfungsleistungen
+select * 
+from student as s
+inner join leistung as l
+	on s.student_id = l.student_id
+inner join pruefung as p
+	on p.pruefung_id = l.pruefung_id
+order by s.student_id, p.pnr;
+
+-- Ermittlung der Durchschnittsnoten der Prüfungen
+select p.pruefung_id, p.bezeichnung, p.pnr, round(avg(note)/100, 2) as durchschnittsnote
+from pruefung as p
+inner join leistung as l
+on p.pruefung_id = l.pruefung_id
+group by p.pruefung_id, p.bezeichnung, p.pnr
+order by pnr;
+
+-- Ermitteln der Anzahl der Prüfungen die der Student jeweils schon bestanden hat
+select matrikelnummer, vorname, nachname, count(l.note) as Anzahl
+from student as s
+left outer join leistung as l
+on s.student_id = l.student_id
+and l.note < 500
+group by matrikelnummer, vorname, nachname
+order by anzahl;
+
+-- Ermitteln der Anzahl bestandenen und nicht bestandenen der Prüfungen der Studenten
+select matrikelnummer, vorname, nachname, 
+	sum(case when l.note < 500 then 1 else 0 end) as Bestanden, 
+	sum(case when l.note = 500 then 1 else 0 end) as NichtBestanden
+from student as s
+left outer join leistung as l
+on s.student_id = l.student_id
+group by matrikelnummer, vorname, nachname
+order by matrikelnummer;
+
+-- Ermitteln in welchen Tabellen es eine Spalte pruefung_id gibt
+select table_name, column_name, data_type
+from information_schema.columns
+where table_catalog = 'schulung'
+and column_name = 'pruefung_id';
+
+-- Ermitteln aller *_id -Spalten
+select table_name, column_name, data_type
+from information_schema.columns
+where table_catalog = 'schulung'
+and column_name like '%\_id' -- zuerst mit %_id versuchen -> aber _ ist ja Platzhalter für genau ein Zeichen!!!
---
+++ demodatenbank_createskript.sql
@@ -0,0 +1,64 @@
+﻿/*
+ * Datenmodell zum Beispiel Student/Prüfung
+ */
+create table student (
+   student_id serial primary key,
+   matrikelnummer varchar(10) not null unique,
+   vorname varchar(50),
+   nachname varchar(50),
+   geschlecht char(1)
+);
+
+create table pruefung (
+   pruefung_id serial primary key,
+   bezeichnung varchar(100),
+   pnr int not null unique
+);
+
+create table leistung (
+   student_id int not null references student(student_id),
+   pruefung_id int not null references pruefung(pruefung_id),
+   punkte int not null,
+   note int not null constraint valid_note check (note >= 100 and note <= 500),
+   primary key (student_id, pruefung_id)
+);
+
+/*
+ * Demodaten
+ */
+insert into student (matrikelnummer, vorname, nachname, geschlecht) values 
+('4711', 'Max', 'Mustermann', 'm'),
+('4712', 'Moritz', 'Mustermann', 'm'),
+('4713', 'Maria', 'Mustermann', 'w'),
+('4714', 'Max', 'Musterer', 'm'),
+('4715', 'Maria', 'Muster', 'w'),
+('4716', 'Marianne', 'Musterbauer', 'w');
+
+insert into pruefung (bezeichnung, pnr) values
+('Mathematik 1', 1001),
+('Mathematik 2', 1011),
+('Programmieren 1', 1002),
+('Programmieren 2', 1012),
+('Betriebswirtschaft', 1003),
+('Rechnungswesen', 1021),
+('Betriebssysteme', 1022),
+('Grundlagen der Informatik', 1004),
+('Verteilte Systeme', 1051),
+('IT-Controlling', 1052);
+
+insert into leistung (student_id, pruefung_id, punkte, note) values
+(1, 1, 100, 100),
+(1, 2, 70, 200),
+(1, 3, 71, 170),
+(1, 4, 50, 370),
+(2, 1, 46, 400),
+(2, 2, 88, 100),
+(3, 3, 76, 170),
+(3, 4, 48, 400),
+(2, 5, 101, 100),
+(3, 6, 63, 330),
+(4, 7, 73, 230),
+(4, 8, 10, 500),
+(4, 5, 55, 370),
+(5, 6, 88, 170),
+(5, 7, 31, 500);
